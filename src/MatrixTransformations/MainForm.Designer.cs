d22fb3a17fa6158ba8869c027ce82667fdfd0455
Timon <timonpost@hotmail.nl>
2021-06-07 09:52:05 +0200

    Add labels

--- src/MatrixTransformations/MainForm.Designer.cs
+++ src/MatrixTransformations/MainForm.Designer.cs
@@ -41,6 +41,11 @@
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.MainSplitContainer = new System.Windows.Forms.SplitContainer();
             this.Canvas = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.tableLayoutPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.MainSplitContainer)).BeginInit();
             this.MainSplitContainer.Panel1.SuspendLayout();
@@ -89,7 +94,7 @@
             // 
             this.scale_txt.AutoSize = true;
             this.scale_txt.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.scale_txt.Location = new System.Drawing.Point(84, 0);
+            this.scale_txt.Location = new System.Drawing.Point(390, 0);
             this.scale_txt.Name = "scale_txt";
             this.scale_txt.Size = new System.Drawing.Size(53, 21);
             this.scale_txt.TabIndex = 10;
@@ -99,7 +104,7 @@
             // 
             this.translate_txt.AutoSize = true;
             this.translate_txt.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.translate_txt.Location = new System.Drawing.Point(84, 21);
+            this.translate_txt.Location = new System.Drawing.Point(390, 21);
             this.translate_txt.Name = "translate_txt";
             this.translate_txt.Size = new System.Drawing.Size(53, 21);
             this.translate_txt.TabIndex = 11;
@@ -109,7 +114,7 @@
             // 
             this.rotate_txt.AutoSize = true;
             this.rotate_txt.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.rotate_txt.Location = new System.Drawing.Point(84, 42);
+            this.rotate_txt.Location = new System.Drawing.Point(390, 42);
             this.rotate_txt.Name = "rotate_txt";
             this.rotate_txt.Size = new System.Drawing.Size(53, 21);
             this.rotate_txt.TabIndex = 12;
@@ -119,7 +124,7 @@
             // 
             this.camera_txt.AutoSize = true;
             this.camera_txt.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.camera_txt.Location = new System.Drawing.Point(84, 63);
+            this.camera_txt.Location = new System.Drawing.Point(390, 63);
             this.camera_txt.Name = "camera_txt";
             this.camera_txt.Size = new System.Drawing.Size(53, 21);
             this.camera_txt.TabIndex = 23;
@@ -148,16 +153,36 @@
             this.tableLayoutPanel2.Controls.Add(this.label6, 0, 2);
             this.tableLayoutPanel2.Controls.Add(this.label12, 0, 3);
             this.tableLayoutPanel2.Controls.Add(this.rotate_txt, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label8, 0, 18);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 17);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 16);
+            this.tableLayoutPanel2.Controls.Add(this.label7, 0, 15);
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 14);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 5;
+            this.tableLayoutPanel2.RowCount = 20;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(389, 816);
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(449, 816);
             this.tableLayoutPanel2.TabIndex = 1;
             // 
             // MainSplitContainer
@@ -174,7 +199,7 @@
             // 
             this.MainSplitContainer.Panel2.Controls.Add(this.Canvas);
             this.MainSplitContainer.Size = new System.Drawing.Size(1440, 816);
-            this.MainSplitContainer.SplitterDistance = 389;
+            this.MainSplitContainer.SplitterDistance = 449;
             this.MainSplitContainer.TabIndex = 0;
             // 
             // Canvas
@@ -182,10 +207,60 @@
             this.Canvas.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Canvas.Location = new System.Drawing.Point(0, 0);
             this.Canvas.Name = "Canvas";
-            this.Canvas.Size = new System.Drawing.Size(1047, 816);
+            this.Canvas.Size = new System.Drawing.Size(987, 816);
             this.Canvas.TabIndex = 0;
             this.Canvas.TabStop = false;
             this.Canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 20);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "PgUp/PgDn: decrase / increase z";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(341, 20);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "X/X, y/Y, z/Z: rotate around x-axis, y-axis, z-axis.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(3, 104);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(225, 20);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "s/S: scale (increase, decrease)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(381, 20);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "When “C” is pressed, all variables are reset to default";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(3, 164);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(267, 20);
+            this.label8.TabIndex = 29;
+            this.label8.Text = "When “A” is pressed, start animation";
             // 
             // MainForm
             // 
@@ -221,6 +296,11 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.SplitContainer MainSplitContainer;
         private System.Windows.Forms.PictureBox Canvas;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label1;
     }
 }
 
