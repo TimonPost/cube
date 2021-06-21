95cfcab629882a0d062a6f061afea9422a1c148a
Timon <timonpost@hotmail.nl>
2021-06-21 14:38:33 +0200

    f

--- src/CubeAssignment.Tests/AnimationTests.cs
+++ src/CubeAssignment.Tests/AnimationTests.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Diagnostics;
+using CubeAssignment.Gui;
+using CubeAssignment.Gui.Animation;
+using CubeAssignment.Gui.Animation.Phases;
+using CubeAssignment.Gui.Scene;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CubeAssignment.Tests
 {
@@ -8,7 +14,22 @@ namespace CubeAssignment.Tests
         [TestMethod]
         public void TestAnimationFirstPhase()
         {
-           
+            Mesh mesh = new Mesh(new ArraySegment<Vertex>(), new ArraySegment<int>());
+            Renderer renderer = new Renderer(0, 0);
+            MeshSceneObject meshObject = new MeshSceneObject(mesh, renderer);
+            Camera camera = new Camera();
+
+            var animationState = new CubeAnimationData(meshObject, camera);
+            FirstPhaseScaleDecrease phase = new FirstPhaseScaleDecrease();
+
+            int timesToTick = 20;
+
+            for (int i = 0; i < timesToTick; i++)
+            {
+                phase.Tick(animationState);
+            }
+
+            Assert.AreEqual(camera.Theta, Camera.DEFAULT_THETA - (phase.StepSize * timesToTick));
         }
     }
 }
