3f691b27b0e91eca106902574973e01c9efbfa6e
Timon <timonpost@hotmail.nl>
2021-06-21 15:27:18 +0200

    f

--- src/CubeAssignment.Tests/AnimationTests.cs
+++ src/CubeAssignment.Tests/AnimationTests.cs
@@ -29,7 +29,7 @@ namespace CubeAssignment.Tests
         }
 
         [TestMethod]
-        public void FirstPhaseTheta_Decrease_Test()
+        public void FirstPhase_Theta_Decrease_Test_1()
         {
             FirstPhaseScaleDecrease phase = new FirstPhaseScaleDecrease();
 
@@ -38,12 +38,12 @@ namespace CubeAssignment.Tests
             for (int i = 0; i < timesToTick; i++)
                 phase.Tick(animationState);
 
-            Assert.AreEqual(Math.Round(camera.Theta, 4), Math.Round(Camera.DefaultTheta - (Settings.THETA_STEP_SIZE * timesToTick+1)), 4);
+            Assert.AreEqual(Math.Round(camera.Theta, 4), Math.Round(Camera.DefaultTheta - (Settings.StepSize * timesToTick+1)), 4);
         }
 
 
         [TestMethod]
-        public void FirstPhaseTheta_Increase_Test()
+        public void FirstPhase_Theta_Decrease_Test_2()
         {
             FirstPhaseScaleIncrease phase = new FirstPhaseScaleIncrease();
 
@@ -52,7 +52,93 @@ namespace CubeAssignment.Tests
             for (int i = 0; i < timesToTick; i++)
                 phase.Tick(animationState);
 
-            Assert.AreEqual(Math.Round(camera.Theta, 4), Math.Round(Camera.DefaultTheta - (Settings.THETA_STEP_SIZE * timesToTick + 1)), 4);
+            Assert.AreEqual(Math.Round(camera.Theta, 4), Math.Round(Camera.DefaultTheta - (Settings.StepSize * timesToTick + 1)), 4);
         }
+
+        [TestMethod]
+        public void FirstPhase_Scale_Decrease_Test()
+        {
+            FirstPhaseScaleDecrease phase = new FirstPhaseScaleDecrease();
+
+            int timesToTick = 500;
+
+            for (int i = 0; i < timesToTick; i++)
+                phase.Tick(animationState);
+
+            Assert.IsTrue(meshObject.Scale.x <= 1.0);
+            Assert.IsTrue(meshObject.Scale.y <= 1.0);
+            Assert.IsTrue(meshObject.Scale.z <= 1.0);
+        }
+
+
+        [TestMethod]
+        public void FirstPhase_Scale_Increase_Test()
+        {
+            FirstPhaseScaleIncrease phase = new FirstPhaseScaleIncrease();
+
+            int timesToTick = 500;
+
+            for (int i = 0; i < timesToTick; i++)
+                phase.Tick(animationState);
+
+            Assert.IsTrue(meshObject.Scale.x >= 1.5);
+            Assert.IsTrue(meshObject.Scale.y >= 1.5);
+            Assert.IsTrue(meshObject.Scale.z >= 1.5);
+        }
+
+        [TestMethod]
+        public void SecondPhase_Theta_Decrease_Test_1()
+        {
+            SecondPhaseRotationXDecrease phase = new SecondPhaseRotationXDecrease();
+
+            int timesToTick = 20;
+
+            for (int i = 0; i < timesToTick; i++)
+                phase.Tick(animationState);
+
+            Assert.AreEqual(Math.Round(camera.Theta, 4), Math.Round(Camera.DefaultTheta - (Settings.StepSize * timesToTick + 1)), 4);
+        }
+
+        [TestMethod]
+        public void SecondPhase_Theta_Decrease_Test_2()
+        {
+            SecondPhaseRotationXIncrease phase = new SecondPhaseRotationXIncrease();
+
+            int timesToTick = 20;
+
+            for (int i = 0; i < timesToTick; i++)
+                phase.Tick(animationState);
+
+            Assert.AreEqual(Math.Round(camera.Theta, 4), Math.Round(Camera.DefaultTheta - (Settings.StepSize * timesToTick + 1)), 4);
+        }
+
+        [TestMethod]
+        public void SecondPhase_Rotation_45_degrees_Test()
+        {
+            SecondPhaseRotationXIncrease phase = new SecondPhaseRotationXIncrease();
+
+            int timesToTick = 500;
+
+            for (int i = 0; i < timesToTick; i++)
+                phase.Tick(animationState);
+
+            Assert.IsTrue(meshObject.Rotation.x >= 0);
+        }
+
+        [TestMethod]
+        public void SecondPhase_Rotation_Minus_45_degrees_Test()
+        {
+            SecondPhaseRotationXDecrease phase = new SecondPhaseRotationXDecrease();
+
+            int timesToTick = 500;
+
+            for (int i = 0; i < timesToTick; i++)
+                phase.Tick(animationState);
+
+            Assert.IsTrue(meshObject.Scale.x <= 1.0);
+            Assert.IsTrue(meshObject.Scale.y <= 1.0);
+            Assert.IsTrue(meshObject.Scale.z <= 1.0);
+        }
+
     }
 }
